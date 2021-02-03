73b30ac401c2c0cfc39b60f3b8e640d891640c48
Juliano Nogueira <jotaweb.art@gmail.com>
2021-02-02 22:02:13 -0300

    login

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 
 require_once("vendor/autoload.php");
 
@@ -7,6 +8,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -23,12 +25,45 @@ $app->get('/', function() {
 
 $app->get('/admin/', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
 
 });
+
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+
+			"header"=>false,
+			"footer"=>false
+
+		]);
+		$page->setTpl("login");
+
+	});
+
+
+	$app->post('/admin/login', function()  {
+
+			user::login($_POST["login"], $_POST["password"]);
+			header("Location: /admin");
+			exit;
+
+
+		});
+
+		$app->get('/admin/logout', function() {
+
+			User::logout();
+			header("Location: /admin/login");
+			exit;
+		});
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,7 +9,8 @@ class Page {
     private $tpl;
     private $options = [];
     private $defaults = [ 
-
+        "header"=>true,
+        "footer"=>true,
         "data"=>[]
     ];
 
@@ -28,7 +29,7 @@ class Page {
 
     $this->setData($this->options["data"]);
 
-    $this->tpl->draw("header");
+    if ($this->options['header'] === true)$this->tpl->draw("header");
 
 
     }
@@ -53,7 +54,7 @@ class Page {
 
     public function __destruct(){
 
-        $this->tpl->draw("footer");
+        if ($this->options['header'] === true)$this->tpl->draw("footer");
         
     }
 }
